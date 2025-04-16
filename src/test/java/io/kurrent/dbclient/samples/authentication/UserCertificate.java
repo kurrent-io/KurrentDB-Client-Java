5f09768163b1664aef38119730ad7d8af98eb418
Paulo Borges <paulohtb@hotmail.com>
2025-04-16 01:52:01 -0300

    docs: change connection string (#318)

--- src/test/java/io/kurrent/dbclient/samples/authentication/UserCertificate.java
+++ src/test/java/io/kurrent/dbclient/samples/authentication/UserCertificate.java
@@ -8,7 +8,7 @@ public class UserCertificate {
     private static void tracing() {
         // region client-with-user-certificates
         KurrentDBClientSettings settings = KurrentDBConnectionString
-                .parseOrThrow("esdb://admin:changeit@{endpoint}?tls=true&userCertFile={pathToCaFile}&userKeyFile={pathToKeyFile}");
+                .parseOrThrow("kurrentdb://admin:changeit@{endpoint}?tls=true&userCertFile={pathToCaFile}&userKeyFile={pathToKeyFile}");
         KurrentDBClient client = KurrentDBClient.create(settings);
         // endregion client-with-user-certificates
     }
