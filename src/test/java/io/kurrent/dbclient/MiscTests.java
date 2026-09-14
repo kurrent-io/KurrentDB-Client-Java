cbe7474ceaef672035eb50cee9c3cffb298546c0
w1am <33353798+w1am@users.noreply.github.com>
2026-09-14 17:09:39 +0000

    fix: propagate W3C trace context for unsampled traces in event metadata

--- src/test/java/io/kurrent/dbclient/MiscTests.java
+++ src/test/java/io/kurrent/dbclient/MiscTests.java
@@ -6,5 +6,5 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectPackages("io.kurrent.dbclient.misc")
-@SelectClasses({SubscriptionStreamConsumerTests.class, LeaderRedirectUnitTest.class})
+@SelectClasses({SubscriptionStreamConsumerTests.class, LeaderRedirectUnitTest.class, TracingContextPropagationTests.class})
 public class MiscTests {}
